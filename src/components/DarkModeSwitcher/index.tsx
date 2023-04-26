5db4601d99968935dcd62396f3aa642e5b17be32
taha khaled <tahakhaledabdellah@gmail.com>
2023-04-26 06:22:21 +0200

    dark mode switcher

---
+++ src/components/DarkModeSwitcher/index.tsx
@@ -0,0 +1,27 @@
+import styles from "./darkModeSwitcher.module.scss";
+import MoonIcon from "@/icons/MoonIcon";
+import SunIcon from "@/icons/SunIcon";
+import { useTheme } from "next-themes";
+const ModeSwitcher = () => {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  const handelToggle = () => {
+    setTheme(resolvedTheme == "light" ? "dark" : "light");
+  };
+  return (
+    <div>
+      <input
+        type="checkbox"
+        id="darkMode-toggle"
+        className={styles.input}
+        checked={resolvedTheme === "dark"}
+        onChange={handelToggle}
+      />
+      <label htmlFor="darkMode-toggle" className={styles.label}>
+        <MoonIcon className={styles.moon} />
+        <SunIcon className={styles.sun} />
+      </label>
+    </div>
+  );
+};
+export default ModeSwitcher;
